00dd596249e9e5ec3c9e8e29409420f99472b56d
Nayeon Shin <nayeon.codes@gmail.com>
2022-03-19 21:30:51 -0400

    feat(`App.js`): Create `query` and add more `useEffect`

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((previous) => previous + 1);
+  const [query, setQuery] = useState("");
 
-  console.log("I run all the time");
+  const onClick = () => setCounter((previous) => previous + 1);
+  const onChange = (event) => setQuery(event.target.value);
 
   useEffect(() => {
-    console.log("Call the API...");
+    console.log("I run only once.");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'query' changes.");
+  }, [query]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'query' & 'counter' change.");
+  }, [query, counter]);
 
   return (
     <div>
+      <input
+        onChange={onChange}
+        placeholder="Search here..."
+        type="text"
+        value={query}
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
